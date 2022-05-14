c2d540191954921b09edf93a6bebe6f6100ddab8
Eric Du <ericdu5000@gmail.com>
2022-05-14 18:38:44 -0400

    publish

--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>
 )
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
